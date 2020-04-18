394c7039ed3ed26f5fff583c6794cf47ec947e23
ALPER TOYGAR <ATOYGAR16@ku.edu.tr>
2020-04-18 16:10:42 +0300

    Added content to Tab
    * Changed the Tab layout to BoxLayout

--- src/main/java/ui/Tab.java
+++ src/main/java/ui/Tab.java
@@ -1,16 +1,19 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Tab extends JPanel {
 
     private String title;
     private JLabel titleLabel;
+    private JTextArea content;
 
     public Tab(String title) {
         this.title = title;
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         createAndPlaceTitleLabel();
-
+        createAndPlaceContent("Content");
     }
 
     private void createAndPlaceTitleLabel(){
@@ -18,12 +21,30 @@ public class Tab extends JPanel {
         placeTitleLabel();
     }
 
+    private void createAndPlaceContent(String content){
+        createContent(content);
+        placeContent();
+    }
+
     private void createTitleLabel(){
         titleLabel = new JLabel();
+        titleLabel.setPreferredSize(new Dimension(100, 30));
         titleLabel.setText(title);
     }
 
     private void placeTitleLabel(){
         add(titleLabel);
     }
+
+    private void createContent(String content){
+        this.content = new JTextArea();
+        this.content.setLineWrap(true);
+        this.content.setEditable(false);
+        this.content.setText(content);
+    }
+
+    private void placeContent(){
+        add(content);
+    }
+
 }
